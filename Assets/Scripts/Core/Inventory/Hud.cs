9768f5cc078bcc5f950d2e7efc2eb5f2773c57b1
HonzaMD <md@krkal.org>
2025-02-10 22:12:21 +0100

    Zapojeni konzole

--- Assets/Scripts/Core/Inventory/Hud.cs
+++ Assets/Scripts/Core/Inventory/Hud.cs
@@ -1,6 +1,7 @@
 ﻿using Assets.Scripts.Utils;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -9,6 +10,7 @@ namespace Assets.Scripts.Core.Inventory
     internal class Hud : MonoBehaviour
     {
         private UnityEngine.UIElements.Label fpsLabel;
+        private VisualElement console;
         private float fpsTimeStamp;
         private int framesCount;
 
@@ -16,6 +18,16 @@ namespace Assets.Scripts.Core.Inventory
         {
             var doc = GetComponent<UIDocument>();
             fpsLabel = doc.rootVisualElement.Q<UnityEngine.UIElements.Label>("FpsLabel");
+            console = doc.rootVisualElement.Q<VisualElement>("Console");
+
+            Application.logMessageReceived += Application_logMessageReceived;
+        }
+
+        private void Application_logMessageReceived(string condition, string stackTrace, LogType type)
+        {
+            var label = console.ElementAt(0) as UnityEngine.UIElements.Label;
+            label.text = condition;
+            label.BringToFront();
         }
 
         private void Update()
